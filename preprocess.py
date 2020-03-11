7cde238791fb3a0edcd1c6613f1422f2fbc64aa0
rs1004 <r82.y25@gmail.com>
2020-03-11 13:00:00 +0000

    [add] preprocess.py, labelmap.json

---
+++ preprocess.py
@@ -0,0 +1,62 @@
+from glob import glob
+from PIL import Image
+from multiprocessing import Pool, cpu_count
+from tqdm import tqdm
+import os
+import json
+import numpy as np
+
+ROOT_DIR = '/work'
+DATA_DIR = '/work/data'
+PROCESS_NUM = cpu_count()
+H = 512
+W = 1024
+
+
+def set_label_map():
+    with open(f'{ROOT_DIR}/labelmap.json', 'r') as f:
+        label_map = json.load(f)
+    global LABEL_MAP
+    LABEL_MAP = {f'{r}_{g}_{b}': i for i, k in enumerate(label_map.keys()) for r, g, b in label_map[k]}
+
+
+def preprocess_img(src_dst_path):
+    src_path, dst_path = src_dst_path
+    img = Image.open(src_path).convert('RGB').resize((W, H))
+    img.save(dst_path)
+
+
+def preprocess_anno(src_dst_path):
+    src_path, dst_path = src_dst_path
+    img = np.asarray(Image.open(src_path).convert('RGB').resize((W, H), resample=Image.NEAREST))
+    img = Image.fromarray(
+        np.array(
+            object=[LABEL_MAP[f'{r}_{g}_{b}'] for (r, g, b) in img.reshape(-1, 3).tolist()],
+            dtype=np.uint8
+        ).reshape(H, W)
+    )
+    img.save(dst_path)
+
+
+if __name__ == '__main__':
+    # initialize
+    set_label_map()
+
+    for t in ['train', 'val']:
+        # preprocess: img => downsize
+        img_path_list = sorted(glob(f'{DATA_DIR}/leftImg8bit/{t}/*/*'))
+        dst_path_list = [f'{DATA_DIR}/img/X{i:05}.png' for i in range(len(img_path_list))]
+
+        os.makedirs(f'{DATA_DIR}/img', exist_ok=True)
+        with Pool(processes=PROCESS_NUM) as p:
+            m = p.imap(preprocess_img, zip(img_path_list, dst_path_list))
+            list(tqdm(m, desc=f'preprocess {t} img files', total=len(img_path_list)))
+
+        # preprocess: anno => rgb2lbl, downsize
+        anno_path_list = sorted(glob(f'{DATA_DIR}/gtFine/{t}/*/*_color.png'))
+        dst_path_list = [f'{DATA_DIR}/anno/Y{i:05}.png' for i in range(len(anno_path_list))]
+
+        os.makedirs(f'{DATA_DIR}/anno', exist_ok=True)
+        with Pool(processes=PROCESS_NUM) as p:
+            m = p.imap(preprocess_anno, zip(anno_path_list, dst_path_list))
+            list(tqdm(m, desc=f'preprocess {t} anno files', total=len(anno_path_list)))
