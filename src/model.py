2ede49ca6674676b6485e8804cd369fa9d0c89ea
rs1004 <r82.y25@gmail.com>
2020-03-17 13:18:52 +0000

    [fix] - model を class 化

--- src/model.py
+++ src/model.py
@@ -1,27 +1,31 @@
 import tensorflow as tf
-import math
-from data_generator import DataGenerator
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu', input_shape=(128, 256, 3)),
-    tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
-    # tf.keras.layers.MaxPooling2D(strides=2),
-    tf.keras.layers.Conv2D(filters=8, kernel_size=1, activation='softmax')
-])
 
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(),
-    loss='sparse_categorical_crossentropy',
-    metrics=['sparse_categorical_accuracy']
-)
+class SampleNet:
+    def __init__(self, input_shape, class_num):
+        self.input_shape = input_shape
+        self.class_num = class_num
+        self.model = self.create_model()
 
-data_gen_train = DataGenerator('train')
-data_gen_val = DataGenerator('val')
+    def __call__(self, x):
+        return self.model.predict(x)
 
-ds_train = data_gen_train.get_one_shot_iterator()
+    def optimize(self, ds, epochs, steps, resume=True):
+        # TODO： check pointを参照する処理
+        self.model.fit(ds, epochs=epochs, steps_per_epoch=steps)
 
-model.fit(ds_train, epochs=5, steps_per_epoch=math.ceil(data_gen_train.data_length/data_gen_train.batch_size))
+    def create_model(self):
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu', input_shape=self.input_shape),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+            # tf.keras.layers.MaxPooling2D(strides=2),
+            tf.keras.layers.Conv2D(filters=self.class_num, kernel_size=1, activation='softmax')
+        ])
 
-ds_val = data_gen_val.get_one_shot_iterator()
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(),
+            loss='sparse_categorical_crossentropy',
+            metrics=['sparse_categorical_accuracy']
+        )
 
-model.evaluate(ds_val, steps=math.ceil(data_gen_val.data_length/data_gen_val.batch_size))
+        return model
