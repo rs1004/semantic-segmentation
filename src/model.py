e44a022de5ad1a6466531f1470884bb258a9d3ab
rs1004 <r82.y25@gmail.com>
2020-03-24 13:14:36 +0000

    [fix] - UNet 実装

--- src/model.py
+++ src/model.py
@@ -2,11 +2,13 @@ import tensorflow as tf
 import re
 from pathlib import Path
 from config import Config
+from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, Conv2DTranspose, concatenate
+from tensorflow.keras.models import Model
 
 CONFIG = Config()
 
 
-class SampleNet:
+class UNet:
     def __init__(self, input_shape, class_num):
         self.input_shape = input_shape
         self.class_num = class_num
@@ -33,12 +35,45 @@ class SampleNet:
         self.model.fit(ds, initial_epoch=initial_epoch, epochs=initial_epoch + epochs, steps_per_epoch=steps, callbacks=[ckpt_callback])
 
     def create_model(self):
-        model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu', input_shape=self.input_shape),
-            tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
-            # tf.keras.layers.MaxPooling2D(strides=2),
-            tf.keras.layers.Conv2D(filters=self.class_num, kernel_size=1, activation='softmax')
-        ])
+        inputs = Input(shape=(self.input_shape))
+
+        conv1_1 = Conv2D(filters=16, kernel_size=3, padding='same', activation='relu')(inputs)
+        conv1_2 = Conv2D(filters=16, kernel_size=3, padding='same', activation='relu')(conv1_1)
+        pool1 = MaxPool2D(pool_size=2, strides=2)(conv1_2)
+
+        conv2_1 = Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(pool1)
+        conv2_2 = Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(conv2_1)
+        pool2 = MaxPool2D(pool_size=2, strides=2)(conv2_2)
+
+        conv3_1 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(pool2)
+        conv3_2 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(conv3_1)
+        pool3 = MaxPool2D(pool_size=2, strides=2)(conv3_2)
+
+        conv4_1 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(pool3)
+        conv4_2 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(conv4_1)
+        pool4 = MaxPool2D(pool_size=2, strides=2)(conv4_2)
+
+        conv5_1 = Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')(pool4)
+        conv5_2 = Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')(conv5_1)
+        concated1 = concatenate([conv4_2, Conv2DTranspose(filters=128, kernel_size=2, strides=2, padding='same')(conv5_2)])
+
+        conv6_1 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(concated1)
+        conv6_2 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(conv6_1)
+        concated2 = concatenate([conv3_2, Conv2DTranspose(filters=64, kernel_size=2, strides=2, padding='same')(conv6_2)])
+
+        conv7_1 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(concated2)
+        conv7_2 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(conv7_1)
+        concated3 = concatenate([conv2_2, Conv2DTranspose(filters=32, kernel_size=2, strides=2, padding='same')(conv7_2)])
+
+        conv8_1 = Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(concated3)
+        conv8_2 = Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(conv8_1)
+        concated4 = concatenate([conv1_2, Conv2DTranspose(filters=16, kernel_size=2, strides=2, padding='same')(conv8_2)])
+
+        conv9_1 = Conv2D(filters=16, kernel_size=3, padding='same', activation='relu')(concated4)
+        conv9_2 = Conv2D(filters=16, kernel_size=3, padding='same', activation='relu')(conv9_1)
+        logits = Conv2D(filters=self.class_num, kernel_size=1, padding='same', activation='softmax')(conv9_2)
+
+        model = Model(inputs=inputs, outputs=logits)
 
         model.compile(
             optimizer=tf.keras.optimizers.Adam(),
--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 from data_generator import DataGenerator
-from model import SampleNet
+from model import UNet
 from config import Config
 
 CONFIG = Config()
@@ -7,6 +7,6 @@ CONFIG = Config()
 data_gen_train = DataGenerator('train')
 ds_train = data_gen_train.get_one_shot_iterator()
 
-model = SampleNet(input_shape=(data_gen_train.H, data_gen_train.W, 3), class_num=CONFIG.CLASS_NUM)
+model = UNet(input_shape=(data_gen_train.H, data_gen_train.W, 3), class_num=CONFIG.CLASS_NUM)
 
 model.optimize(ds=ds_train, epochs=CONFIG.EPOCHS, steps=data_gen_train.data_length // CONFIG.BATCH_SIZE)
