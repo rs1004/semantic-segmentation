1d8aff8b2f37dcdc86508b3b06093f90f5aa0dcb
rs1004 <r82.y25@gmail.com>
2020-03-25 10:47:26 +0000

    [fix] 論文準拠

--- src/model.py
+++ src/model.py
@@ -37,40 +37,40 @@ class UNet:
     def create_model(self):
         inputs = Input(shape=(self.input_shape))
 
-        conv1_1 = Conv2D(filters=16, kernel_size=3, padding='same', activation='relu')(inputs)
-        conv1_2 = Conv2D(filters=16, kernel_size=3, padding='same', activation='relu')(conv1_1)
+        conv1_1 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(inputs)
+        conv1_2 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(conv1_1)
         pool1 = MaxPool2D(pool_size=2, strides=2)(conv1_2)
 
-        conv2_1 = Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(pool1)
-        conv2_2 = Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(conv2_1)
+        conv2_1 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(pool1)
+        conv2_2 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(conv2_1)
         pool2 = MaxPool2D(pool_size=2, strides=2)(conv2_2)
 
-        conv3_1 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(pool2)
-        conv3_2 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(conv3_1)
+        conv3_1 = Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')(pool2)
+        conv3_2 = Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')(conv3_1)
         pool3 = MaxPool2D(pool_size=2, strides=2)(conv3_2)
 
-        conv4_1 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(pool3)
-        conv4_2 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(conv4_1)
+        conv4_1 = Conv2D(filters=512, kernel_size=3, padding='same', activation='relu')(pool3)
+        conv4_2 = Conv2D(filters=512, kernel_size=3, padding='same', activation='relu')(conv4_1)
         pool4 = MaxPool2D(pool_size=2, strides=2)(conv4_2)
 
-        conv5_1 = Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')(pool4)
-        conv5_2 = Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')(conv5_1)
-        concated1 = concatenate([conv4_2, Conv2DTranspose(filters=128, kernel_size=2, strides=2, padding='same')(conv5_2)])
+        conv5_1 = Conv2D(filters=1024, kernel_size=3, padding='same', activation='relu')(pool4)
+        conv5_2 = Conv2D(filters=1024, kernel_size=3, padding='same', activation='relu')(conv5_1)
+        concated1 = concatenate([conv4_2, Conv2DTranspose(filters=512, kernel_size=2, strides=2, padding='same')(conv5_2)])
 
-        conv6_1 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(concated1)
-        conv6_2 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(conv6_1)
-        concated2 = concatenate([conv3_2, Conv2DTranspose(filters=64, kernel_size=2, strides=2, padding='same')(conv6_2)])
+        conv6_1 = Conv2D(filters=512, kernel_size=3, padding='same', activation='relu')(concated1)
+        conv6_2 = Conv2D(filters=512, kernel_size=3, padding='same', activation='relu')(conv6_1)
+        concated2 = concatenate([conv3_2, Conv2DTranspose(filters=256, kernel_size=2, strides=2, padding='same')(conv6_2)])
 
-        conv7_1 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(concated2)
-        conv7_2 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(conv7_1)
-        concated3 = concatenate([conv2_2, Conv2DTranspose(filters=32, kernel_size=2, strides=2, padding='same')(conv7_2)])
+        conv7_1 = Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')(concated2)
+        conv7_2 = Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')(conv7_1)
+        concated3 = concatenate([conv2_2, Conv2DTranspose(filters=128, kernel_size=2, strides=2, padding='same')(conv7_2)])
 
-        conv8_1 = Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(concated3)
-        conv8_2 = Conv2D(filters=32, kernel_size=3, padding='same', activation='relu')(conv8_1)
-        concated4 = concatenate([conv1_2, Conv2DTranspose(filters=16, kernel_size=2, strides=2, padding='same')(conv8_2)])
+        conv8_1 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(concated3)
+        conv8_2 = Conv2D(filters=128, kernel_size=3, padding='same', activation='relu')(conv8_1)
+        concated4 = concatenate([conv1_2, Conv2DTranspose(filters=64, kernel_size=2, strides=2, padding='same')(conv8_2)])
 
-        conv9_1 = Conv2D(filters=16, kernel_size=3, padding='same', activation='relu')(concated4)
-        conv9_2 = Conv2D(filters=16, kernel_size=3, padding='same', activation='relu')(conv9_1)
+        conv9_1 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(concated4)
+        conv9_2 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')(conv9_1)
         logits = Conv2D(filters=self.class_num, kernel_size=1, padding='same', activation='softmax')(conv9_2)
 
         model = Model(inputs=inputs, outputs=logits)
