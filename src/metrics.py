9a1249a0b3f6fc59bf69b5b972c945c138834ce5
rs1004 <r82.y25@gmail.com>
2020-04-08 22:15:46 +0900

    [fix] class単位のmetric追加

--- src/metrics.py
+++ src/metrics.py
@@ -9,10 +9,17 @@ CONFIG = Config()
 
 def get_metrics():
     metrics = []
-    # for i in range(CONFIG.CLASS_NUM):
-    #     f = partial(sparse_class_average_iou, class_id=i)
-    #     f.__name__ = f'average_iou_class_{i}'
-    #     metrics.append(f)
+    class_fs = {
+        'average_iou': sparse_class_average_iou,
+        'average_precision': sparse_class_average_precision,
+        'average_recall': sparse_class_average_recall,
+        'average_f1_score': sparse_class_f1_score
+    }
+    for class_f_name, class_f in class_fs.items():
+        for i in range(CONFIG.CLASS_NUM):
+            f = partial(class_f, class_id=i)
+            f.__name__ = f'{class_f_name}_class_{i}'
+            metrics.append(f)
     metrics.append(sparse_mean_iou)
     metrics.append(sparse_f1_score)
     metrics.append(sparse_categorical_accuracy)
@@ -49,6 +56,17 @@ def sparse_mean_iou(y_true, y_pred):
     return K.mean(average_ious)
 
 
+def sparse_class_average_precision(y_true, y_pred, class_id):
+    y_true, y_pred = _convert(y_true=y_true, y_pred=y_pred)
+
+    tp = K.sum(y_true[:, :, :, class_id] * y_pred[:, :, :, class_id], axis=(1, 2))
+    tp_fp = K.sum(y_pred[:, :, :, class_id], axis=(1, 2))
+
+    precisions = tp / (tp_fp + K.epsilon())
+
+    return K.mean(precisions)
+
+
 def sparse_mean_average_precision(y_true, y_pred):
     y_true, y_pred = _convert(y_true=y_true, y_pred=y_pred)
 
@@ -60,6 +78,17 @@ def sparse_mean_average_precision(y_true, y_pred):
     return K.mean(average_precisions)
 
 
+def sparse_class_average_recall(y_true, y_pred, class_id):
+    y_true, y_pred = _convert(y_true=y_true, y_pred=y_pred)
+
+    tp = K.sum(y_true[:, :, :, class_id] * y_pred[:, :, :, class_id], axis=(1, 2))
+    tp_fn = K.sum(y_true[:, :, :, class_id], axis=(1, 2))
+
+    recalls = tp / (tp_fn + K.epsilon())
+
+    return K.mean(recalls)
+
+
 def sparse_mean_average_recall(y_true, y_pred):
     y_true, y_pred = _convert(y_true=y_true, y_pred=y_pred)
 
@@ -69,6 +98,19 @@ def sparse_mean_average_recall(y_true, y_pred):
     recalls = tp / (tp_fn + K.epsilon())
     average_recalls = K.mean(recalls, axis=0)
     return K.mean(average_recalls)
+
+
+def sparse_class_f1_score(y_true, y_pred, class_id):
+    y_true, y_pred = _convert(y_true=y_true, y_pred=y_pred)
+
+    tp = K.sum(y_true[:, :, :, class_id] * y_pred[:, :, :, class_id], axis=(1, 2))
+    tp_fp = K.sum(y_pred[:, :, :, class_id], axis=(1, 2))
+    tp_fn = K.sum(y_true[:, :, :, class_id], axis=(1, 2))
+
+    precision = K.mean(tp / (tp_fp + K.epsilon()))
+    recall = K.mean(tp / (tp_fn + K.epsilon()))
+
+    return 2 * precision * recall / (precision + recall)
 
 
 def sparse_f1_score(y_true, y_pred):
